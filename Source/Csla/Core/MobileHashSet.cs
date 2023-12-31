acf0dccd6621f312dde59303cd9082585e0c6cbd
George Christodoulides <george@mycontent.gr>
2023-12-31 18:36:59 +0100

    #3643 Adding support for MobileHashSet, MobileSortedDictionary, MobileSortedList, MobileSortedSet

---
+++ Source/Csla/Core/MobileHashSet.cs
@@ -0,0 +1,156 @@
+//-----------------------------------------------------------------------
+// <copyright file="MobileHashSet.cs" company="Marimer LLC">
+//     Copyright (c) Marimer LLC. All rights reserved.
+//     Website: https://cslanet.com
+// </copyright>
+// <summary>Implements a HashSet that is serializable using</summary>
+//-----------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using Csla.Serialization.Mobile;
+
+namespace Csla.Core
+{
+  /// <summary>
+  /// Implements a HashSet that is serializable using
+  /// the SerializationFormatterFactory.GetFormatter().
+  /// </summary>
+  /// <typeparam name="T">
+  /// Type of object contained in the HashSet.
+  /// </typeparam>
+  [Serializable]
+  public class MobileHashSet<T> : HashSet<T>, IMobileObject
+  {
+    /// <summary>
+    /// Creates an instance of the type.
+    /// </summary>
+    public MobileHashSet() : base() { }
+    
+    /// <summary>
+    /// Creates an instance of the type.
+    /// </summary>
+    /// <param name="collection">Data to add to set.</param>
+    public MobileHashSet(IEnumerable<T> collection) : base(collection) { }
+
+    /// <summary>
+    /// Creates an instance of the type.
+    /// </summary>
+    /// <param name="comparer">Equality comparer to use.</param>
+    public MobileHashSet(IEqualityComparer<T> comparer) : base(comparer) { }
+
+#if (NET47_OR_GREATER || NETSTANDARD2_1_OR_GREATER)
+    /// <summary>
+    /// Creates an instance of the type.
+    /// </summary>
+    /// <param name="capacity">Capacity of the set.</param>
+    public MobileHashSet(Int32 capacity) : base(capacity) { }
+#endif
+
+    /// <summary>
+    /// Creates an instance of the type.
+    /// </summary>
+    /// <param name="collection">Data to add to set.</param>
+    /// <param name="comparer">Equality comparer to use.</param>
+    public MobileHashSet(IEnumerable<T> collection, IEqualityComparer<T> comparer) : base(collection, comparer) { }
+
+#if (NET47_OR_GREATER || NETSTANDARD2_1_OR_GREATER)
+    /// <summary>
+    /// Creates an instance of the type.
+    /// </summary>
+    /// <param name="capacity">Capacity of the set.</param>
+    /// <param name="comparer">Equality comparer to use.</param>
+    public MobileHashSet(Int32 capacity, IEqualityComparer<T> comparer) : base(capacity, comparer) { }
+#endif    
+
+    #region IMobileObject Members
+
+    void IMobileObject.GetChildren(SerializationInfo info, MobileFormatter formatter)
+    {
+      OnGetChildren(info, formatter);
+    }
+
+    void IMobileObject.GetState(SerializationInfo info)
+    {
+      OnGetState(info);
+    }
+
+    /// <summary>
+    /// Override this method to add extra field values to
+    /// the serialization stream.
+    /// </summary>
+    /// <param name="info">Object containing field values.</param>
+    protected virtual void OnGetState(SerializationInfo info)
+    {
+    }
+
+    private const string _valuePrefix = "v";
+
+    /// <summary>
+    /// Override this method to manually serialize child objects
+    /// contained within the current object.
+    /// </summary>
+    /// <param name="info">Object containing serialized values.</param>
+    /// <param name="formatter">Reference to the current SerializationFormatterFactory.GetFormatter().</param>
+    protected virtual void OnGetChildren(SerializationInfo info, MobileFormatter formatter)
+    {
+      bool mobileChildren = typeof(IMobileObject).IsAssignableFrom(typeof(T));
+      int count = 0;
+      foreach (T child in this)
+      {
+        if (mobileChildren)
+        {
+          SerializationInfo si = formatter.SerializeObject(child);
+          info.AddChild(_valuePrefix + count, si.ReferenceId);
+        }
+        else
+        {
+          info.AddValue(_valuePrefix + count, child);
+        }
+        count++;
+      }
+      info.AddValue("count", count);
+    }
+
+    void IMobileObject.SetState(SerializationInfo info)
+    {
+      OnSetState(info);
+    }
+
+    void IMobileObject.SetChildren(SerializationInfo info, MobileFormatter formatter)
+    {
+      OnSetChildren(info, formatter);
+    }
+
+    /// <summary>
+    /// Override this method to retrieve extra field values to
+    /// the serialization stream.
+    /// </summary>
+    /// <param name="info">Object containing field values.</param>
+    protected virtual void OnSetState(SerializationInfo info) { }
+
+    /// <summary>
+    /// Override this method to manually deserialize child objects
+    /// from data in the serialization stream.
+    /// </summary>
+    /// <param name="info">Object containing serialized values.</param>
+    /// <param name="formatter">Reference to the current SerializationFormatterFactory.GetFormatter().</param>
+    protected virtual void OnSetChildren(SerializationInfo info, MobileFormatter formatter)
+    {
+      bool mobileChildren = typeof(IMobileObject).IsAssignableFrom(typeof(T));
+      int count = info.GetValue<int>("count");
+
+      for (int index = 0; index < count; index++)
+      {
+        T value;
+        if (mobileChildren)
+          value = (T)formatter.GetObject(info.Children[_valuePrefix + index].ReferenceId);
+        else
+          value = info.GetValue<T>(_valuePrefix + index);
+
+        Add(value);
+      }
+    }
+
+    #endregion
+  }
+}
---
+++ Source/Csla/Core/MobileSortedList.cs
@@ -0,0 +1,157 @@
+//-----------------------------------------------------------------------
+// <copyright file="MobileSortedList.cs" company="Marimer LLC">
+//     Copyright (c) Marimer LLC. All rights reserved.
+//     Website: https://cslanet.com
+// </copyright>
+// <summary>Defines a sorted list that can be serialized through</summary>
+//-----------------------------------------------------------------------
+using System.Collections.Generic;
+using System.Linq;
+using Csla.Serialization.Mobile;
+using System;
+using System.Reflection;
+using Csla.Reflection;
+
+namespace Csla.Core
+{
+  /// <summary>
+  /// Defines a sorted list that can be serialized through
+  /// the SerializationFormatterFactory.GetFormatter().
+  /// </summary>
+  /// <typeparam name="K">Key value: any primitive or IMobileObject type.</typeparam>
+  /// <typeparam name="V">Value: any primitive or IMobileObject type.</typeparam>
+  [Serializable()]
+  public class MobileSortedList<K, V> : SortedList<K, V>, IMobileObject where K : notnull
+  {
+    private bool _keyIsMobile;
+    private bool _valueIsMobile;
+
+    /// <summary>
+    /// Creates an instance of the type.
+    /// </summary>
+    public MobileSortedList()
+    {
+      DetermineTypes();
+    }
+
+    /// <summary>
+    /// Creates an instance of the object based
+    /// on the supplied list, whose elements
+    /// are copied to the new list.
+    /// </summary>
+    /// <param name="comparer">The comparer to use when comparing keys.</param>
+    public MobileSortedList(IComparer<K> comparer)
+      : base(comparer)
+    {
+      DetermineTypes();
+    }
+
+    /// <summary>
+    /// Creates an instance of the object based
+    /// on the supplied list, whose elements
+    /// are copied to the new list.
+    /// </summary>
+    /// <param name="dict">Source list.</param>
+    public MobileSortedList(IDictionary<K, V> dict)
+      : base(dict)
+    {
+      DetermineTypes();
+    }
+
+    /// <summary>
+    /// Creates an instance of the object based
+    /// on the supplied list, whose elements
+    /// are copied to the new list.
+    /// </summary>
+    /// <param name="dict">Source list.</param>
+    /// <param name="comparer">The comparer to use when comparing keys.</param>
+    public MobileSortedList(IDictionary<K, V> dict, IComparer<K> comparer)
+      : base(dict, comparer)
+    {
+      DetermineTypes();
+    }
+
+    /// <summary>
+    /// Gets a value indicating whether the
+    /// list contains the specified key
+    /// value.
+    /// </summary>
+    /// <param name="key">Key value</param>
+    public bool Contains(K key)
+    {
+      return base.ContainsKey(key);
+    }
+
+    private void DetermineTypes()
+    {
+      _keyIsMobile = typeof(Csla.Serialization.Mobile.IMobileObject).IsAssignableFrom(typeof(K));
+      _valueIsMobile = typeof(Csla.Serialization.Mobile.IMobileObject).IsAssignableFrom(typeof(V));
+    }
+
+    #region IMobileObject Members
+
+    private const string _keyPrefix = "k";
+    private const string _valuePrefix = "v";
+
+    void IMobileObject.GetState(SerializationInfo info)
+    {
+      info.AddValue("count", this.Keys.Count);
+    }
+
+    void IMobileObject.GetChildren(SerializationInfo info, MobileFormatter formatter)
+    {
+      int count = 0;
+      foreach (var key in this.Keys)
+      {
+        if (_keyIsMobile)
+        {
+          SerializationInfo si = formatter.SerializeObject(key);
+          info.AddChild(_keyPrefix + count, si.ReferenceId);
+        }
+        else
+        {
+          info.AddValue(_keyPrefix + count, key);
+        }
+
+        if (_valueIsMobile)
+        {
+          SerializationInfo si = formatter.SerializeObject(this[key]);
+          info.AddChild(_valuePrefix + count, si.ReferenceId);
+        }
+        else
+        {
+          V value = this[key];
+          info.AddValue(_valuePrefix + count, value);
+        }
+        count++;
+      }
+    }
+
+    void IMobileObject.SetState(SerializationInfo info)
+    { }
+
+    void IMobileObject.SetChildren(SerializationInfo info, MobileFormatter formatter)
+    {
+      int count = info.GetValue<int>("count");
+
+      for (int index = 0; index < count; index++)
+      {
+        K key;
+        if (_keyIsMobile)
+          key = (K)formatter.GetObject(info.Children[_keyPrefix + index].ReferenceId);
+        else
+          key = info.GetValue<K>(_keyPrefix + index);
+
+        V value;
+        if (_valueIsMobile)
+          value = (V)formatter.GetObject(info.Children[_valuePrefix + index].ReferenceId);
+        else
+          value = info.GetValue<V>(_valuePrefix + index);
+
+        Add(key, value);
+      }
+    }
+
+    #endregion
+  }
+}
